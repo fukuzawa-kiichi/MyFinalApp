797f853c9540c3a9387e1fec93f91d53d2459702
VERTEX24 <vertex24@VERTEX24noMacBook-Air.local>
2019-08-19 14:57:41 +0900

    first commit

---
+++ MyFinalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFinalApp
+//
+//  Created by VERTEX24 on 2019/08/19.
+//  Copyright © 2019 VERTEX24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
