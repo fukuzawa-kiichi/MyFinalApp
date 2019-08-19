9f631df5986cae798749806562d125db5f1b14be
VERTEX24 <vertex24@VERTEX24noMacBook-Air.local>
2019-08-19 23:01:23 +0900

    投稿画面作成中

---
+++ MyFinalApp/Post/PostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostViewController.swift
+//  MyFinalApp
+//
+//  Created by VERTEX24 on 2019/08/19.
+//  Copyright © 2019 VERTEX24. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
