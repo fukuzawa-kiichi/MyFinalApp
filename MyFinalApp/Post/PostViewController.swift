3e3762bd00c1685fc9e1ec150885cb77f1b0ed1d
VERTEX24 <vertex24@VERTEX24noMacBook-Air.local>
2019-08-20 14:24:25 +0900

    投稿画面のレイアウトと紐付け

--- MyFinalApp/Post/PostViewController.swift
+++ MyFinalApp/Post/PostViewController.swift
@@ -10,21 +10,47 @@ import UIKit
 
 class PostViewController: UIViewController {
 
+    // すべてtユーザーに記入してもらう
+    // 投稿する画像
+    @IBOutlet weak var imageView: UIImageView!
+    // お店の名前
+    @IBOutlet weak var shopTextField: UITextField!
+    // お店の場所
+    @IBOutlet weak var placeTextField: UITextField!
+    // デースドリンクの名前
+    @IBOutlet weak var baseTextField: UITextField!
+    // トッピング(最大5個まで)
+    // トッピング1つ目
+    @IBOutlet weak var toppingTextField: UITextField!
+    // 追加1項目
+    @IBOutlet weak var p1TextField: UITextField!
+    // 追加2項目
+    @IBOutlet weak var p2TextField: UITextField!
+    // 追加3項目
+    @IBOutlet weak var p3TextField: UIStackView!
+    // 追加4項目
+    @IBOutlet weak var p4TextField: UITextField!
+    // 甘さと氷の量
+    @IBOutlet weak var iceTextField: UITextField!
+    // 価格
+    @IBOutlet weak var priceTextField: UITextField!
+    // 待ち時間
+    @IBOutlet weak var timeTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // 投稿ボタン
+    @IBAction func postAll(_ sender: Any) {
     }
-    */
-
+    
+    // 投稿をやめるボタン
+    @IBAction func cancelAll(_ sender: Any) {
+    }
+    
+    // トッピングの項目を増やすボタン
+    @IBAction func plusButton(_ sender: UIButton) {
+    }
+    
 }
